a85e6b34b18ce04df14a2e2cd5e9c92eb6078cb1
admin <admin@esprit>
2014-02-18 15:52:39 +0100

    by nada

--- src/javaappldao/JavaApplDAO.java
+++ src/javaappldao/JavaApplDAO.java
@@ -16,7 +16,7 @@ public class JavaApplDAO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here riqdh nada
+        // TODO code application logic here riqdh nada vvvvv
     }
     
 }
